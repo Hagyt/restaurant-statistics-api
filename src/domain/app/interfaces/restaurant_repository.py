96d9209a725315ca68e10cededf8e68be6716819
Hagyt <davidurregosierra12@hotmail.com>
2023-10-18 18:32:38 -0500

    feat(restaurant_repository_interface): Add a restaurant repository interface to restrict the methods needed

---
+++ src/domain/app/interfaces/restaurant_repository.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from src.domain.model.restaurant import Restaurant
+
+
+class RestaurantRepository(ABC):
+    @abstractmethod
+    def create(self, restaurant: Restaurant) -> Restaurant:
+        pass
+
+    
+    @abstractmethod
+    def update(self, object_id: str, data: dict) -> Restaurant:
+        pass
+
+    
+    @abstractmethod
+    def delete(self, object_id: str) -> Restaurant:
+        pass
+
+
+    @abstractmethod
+    def get_all(self, query_params=None) -> List[Restaurant]:
+        pass
