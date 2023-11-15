659cadc711f6a847774363b8545e40d411079f98
Hagyt <davidurregosierra12@hotmail.com>
2023-11-15 15:58:36 -0500

    feat(statistics_endpoint): Add endpoint to get the statistics from specific point

--- src/external/rest_api/controllers/restaurant.py
+++ src/external/rest_api/controllers/restaurant.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-from src.external.rest_api.middleware import post_data_required
+from src.external.rest_api.middleware import post_data_required, qparams_required
 from src.external.rest_api.responses import create_response
-from src.external.rest_api.schemas import RestaurantSchema
+from src.external.rest_api.schemas import RestaurantSchema, SearchAreaSchema, StatisticsSchema
 from src.external.persistence.repositories import SqliteRestaurantRepository
 from src.domain.app.restaurant_service import RestaurantService
 
@@ -55,3 +55,20 @@ def remove_restaurant(id):
     restaurant_removed = restaurant_service.remove_restaurant(id)
     return create_response(restaurant_removed, RestaurantSchema)
     
+
+@blueprint.route("/restaurants/statistics")
+@qparams_required
+def get_statistics(qparams):
+    # Validated query params
+    search_area_schema = SearchAreaSchema()
+    validated_data = search_area_schema.load(qparams)
+
+    # Get statistics using service
+    restaurant_repository = SqliteRestaurantRepository()
+    restaurant_service = RestaurantService(restaurant_repository)
+    statistics = restaurant_service.get_area_restaurants_statistics(
+        validated_data.get("latitude"),
+        validated_data.get("longitude"),
+        validated_data.get("radius")
+        )
+    return create_response(statistics, StatisticsSchema)
