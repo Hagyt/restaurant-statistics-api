035ebf4c8f724db58130ac3d317ac27e06ac7ea3
David Urrego <david.urrego@trycore.com>
2023-12-22 19:19:50 -0500

    feat(bulk_restaurants): Add a command to insert the seed data to the restaurants table

--- src/management.py
+++ src/management.py
@@ -1,7 +1,9 @@
+import pandas as pd
 from sqlalchemy import inspect
 from flask_migrate import Migrate
 
 from src.external.persistence.databases import sqlalchemy_db as db
+from src.external.persistence.repositories import SqlalchemyRestaurantRepository
 
 
 migrate = Migrate()
@@ -19,5 +21,26 @@ def setup_management(app):
     @app.cli.command("print_config")
     def print_config():
         print(app.config)
+
+
+    @app.cli.command("bulk_seed_restaurant_data")
+    def bulk_seed_restaurant_data():
+        restaurant_repository = SqlalchemyRestaurantRepository()
+        ## Read csv
+        df = pd.read_csv("resources/restaurantes.csv")
+
+        ## Create geom property
+        def create_geom_property(row):
+            row["geom"] = {
+                "lng": row["lng"],
+                "lat": row["lat"]
+            }
+            return row
+        
+        data = list(map(create_geom_property, df.to_dict("records")))
+        
+        for r in data:
+            restaurant_created = restaurant_repository.create(r)
+            print(f"Restaurant created: {restaurant_created.__str__()}")
         
     return app
