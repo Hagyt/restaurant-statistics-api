2d8b73fa1ddd01db7d969f3e0ad9232916fd8755
David Urrego <david.urrego@trycore.com>
2023-12-23 19:13:29 -0500

    feat(logging): Add loggers using the logging lib of python

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
+import sys
+import logging
 from src.create_app import create_app
 from src.config import Config
 
 
+logger = logging.getLogger(__name__)
 app = create_app(Config)
+logging.basicConfig(stream=sys.stderr)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/create_app.py
+++ src/create_app.py
@@ -2,12 +2,14 @@ from flask import Flask
 
 from src.external.rest_api import setup_blueprints
 from src.external.persistence.databases import setup_sqlalchemy
+from src.logging import setup_logging
 from src.management import setup_management
 
 
 def create_app(config):
     app = Flask(__name__.split('.')[0])
     app.config.from_object(config)
+    app = setup_logging(app)
     app = setup_blueprints(app)
     app = setup_sqlalchemy(app)
     app = setup_management(app)
